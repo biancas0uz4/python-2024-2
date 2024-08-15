4d99c7135a1cf536b1389d423eec6858c88894dd
biancas0uz4 <bianca.rsouza@sptech.school>
2024-08-15 11:58:03 -0300

    Primeira aula de python

---
+++ pythonProject/aula02.py
@@ -0,0 +1,21 @@
+a = 10
+b = 5
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+resto = a % b
+
+print(type(soma))
+print(str(soma))
+print(subtracao)
+print(multiplicacao)
+print(float(divisao))
+print(resto)
+#print(resto)
+
+# str converte int pra string
+
+# x = '1'
+# soma2 = int(x) + 1
+# print(soma2)
